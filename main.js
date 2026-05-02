71978adf12d23fc99fe112d13d133de8a1769361
Kaito.Tamura <tamura.kaito2025@gmail.com>
2026-05-03 07:17:00 +0900

    formのsubmit時にpreventcDefaultを追加

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const weatherForm = document.querySelector('#weather-form');
+const cityInput = document.querySelector('#city-input');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
